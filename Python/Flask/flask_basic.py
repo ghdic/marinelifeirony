b99bddb150cc1e084be4d4388c86b69947973cfc
ghdic <ghdic@naver.com>
2019-07-31 23:36:48 +0900

    머야 왜 다 커밋안대 ㅠ

--- Python/Flask/flask_basic.py
+++ Python/Flask/flask_basic.py
@@ -14,10 +14,10 @@ def about():
 if __name__ == '__main__':
     app.run(debug=True)
 
-# window cmd 창에서 set FLASK_APP=test.py -> 변수 설정
-# linux export FLASK_APP=test.py
+# window cmd 창에서 set FLASK_APP=flask_basic.py -> 변수 설정
+# linux export FLASK_APP=flask_basic.py
 # flask run
 # html 태그 먹힘
 # flask run 으로할땐 메인으로 실행 x
-# python test.py 로 로컬로 실행할때만 메인으로 들어감감
+# python flask_basic.py 로 로컬로 실행할때만 메인으로 들어감감
 # 디버그 모드로 하면 수정하는거 실시간 반영되는듯
