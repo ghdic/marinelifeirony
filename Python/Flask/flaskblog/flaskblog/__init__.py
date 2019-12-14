fb10ffb5e2080ff9e35b1d084de55b475e5c36a1
ghdic <ghdic@naver.com>
2019-12-15 07:17:47 +0900

    너무 오래 커밋을 안했네 이거;; 알고리즘 추가 & python 모듈 몇개 추가

---
+++ Python/Flask/flaskblog/flaskblog/__init__.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = '1234567890123456' # import secrets secrets.token_hex(16)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USER_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')  # 윈도우 시스템 환경변수 설정 직접 입력도 가능
+mail = Mail(app)
+from flaskblog import route
