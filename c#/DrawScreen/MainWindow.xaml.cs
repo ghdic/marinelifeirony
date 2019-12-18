e8504c750f7642800a24e387eaced2ba4345571b
ghdic <ghdic@naver.com>
2019-12-19 00:19:08 +0900

    save/load, info display, clear, minimize 구현

--- c#/DrawScreen/MainWindow.xaml.cs
+++ c#/DrawScreen/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Ink;
 
 namespace DrawScreen
 {
@@ -23,6 +17,9 @@ namespace DrawScreen
         private Point _lastMousePosition;
         private bool _isDraging = false;
         private Border _selectedColor;
+        private string _staticInfo = "";
+        private bool _displayingInfo;
+        private bool _usingEraser;
 
         public MainWindow()
         {
@@ -30,7 +27,30 @@ namespace DrawScreen
             SelectColor(DefaultColor);
         }
 
-       private void SelectColor(Border border)
+        private async void Display(string info)
+        {
+            InfoBox.Text = info;
+            _displayingInfo = true;
+            await InfoDisplayTimeUp(new Progress<string>(box => InfoBox.Text = box));
+        }
+
+        private Task InfoDisplayTimeUp(IProgress<string> box)
+        {
+            return Task.Run(() =>
+            {
+                Task.Delay(2000).Wait();
+                box.Report(_staticInfo);
+                _displayingInfo = false;
+            });
+        }
+
+        private void SetStaticInfo(string info) {
+            _staticInfo = info;
+            if (!_displayingInfo)
+                InfoBox.Text = _staticInfo;
+        }
+
+        private void SelectColor(Border border)
         {
             var solidColorBrush = border.Background as SolidColorBrush;
             if (solidColorBrush == null) return;
@@ -41,6 +61,21 @@ namespace DrawScreen
                 _selectedColor.BorderThickness = new Thickness(0);
             _selectedColor = border;
 
+        }
+
+        private static string GenerateFileName() {
+            return DateTime.Now.ToString("yyyyMMdd-HHmmss") + ".icv";
+        }
+
+        private static string GetFileNameFromPath(string path)
+        {
+            return Path.GetFileName(path);
+        }
+
+        private static FileStream GetFileStream(string filename){
+            if (!Directory.Exists("Save"))
+                Directory.CreateDirectory("Save");
+            return new FileStream("Save\\" + filename, FileMode.OpenOrCreate);
         }
 
         #region Controls
@@ -62,6 +97,49 @@ namespace DrawScreen
         private void CloseButton_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Application.Current.Shutdown(0);
+        }
+
+        private void EraserButton_MouseDown(object sender, MouseButtonEventArgs e) {
+            if (_usingEraser) {
+                MainInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                EraserButton.Background = new SolidColorBrush(Color.FromArgb(0x4C, 0, 0, 0));
+                _usingEraser = false;
+                SetStaticInfo("");
+            } else {
+                MainInkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+                EraserButton.Background = new SolidColorBrush(Color.FromArgb(80, 255, 255, 255));
+                _usingEraser = true;
+                SetStaticInfo("Eraser Mode");
+            }
+
+        }
+
+        private void UndoButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Display("Function Unfinished");
+        }
+
+        private void SaveButton_MouseDown(object sender, MouseButtonEventArgs e) {
+            var fn = "Test.icv";
+            using (var s = GetFileStream(fn))
+                MainInkCanvas.Strokes.Save(s);
+            Display("Save to " + fn);
+        }
+
+        private void LoadButton_MouseDown(object sender, MouseButtonEventArgs e) {
+            var fn = "Test.icv";
+            using (var s = GetFileStream(fn))
+                MainInkCanvas.Strokes = new StrokeCollection(s);
+            Display("Loaded from " + fn);
+        }
+
+        private void MinimizeButton_MouseDown(object sender, MouseButtonEventArgs e) {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ClearButton_MouseDown(object sender, MouseButtonEventArgs e) {
+            MainInkCanvas.Strokes.Clear();
+            Display("Cleared");
         }
 
         #region Drag
@@ -105,5 +183,7 @@ namespace DrawScreen
             EndDrag();
         }
         #endregion
+
+        
     }
 }
