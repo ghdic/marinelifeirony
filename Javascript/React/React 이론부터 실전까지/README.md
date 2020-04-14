fcf42dff0386390d8a2d6a2f02602a866d7ab678
ghdic <ghdic@naver.com>
2020-04-14 12:57:33 +0900

    react 시작!

---
+++ Javascript/React/React 이론부터 실전까지/README.md
@@ -0,0 +1,14 @@
+# 강의 주소
+
+[React Js 이론부터 실전까지](https://www.youtube.com/watch?v=s2knmog2j1U&list=PLRx0vPvlEmdCED62ZIWCbI-6G_jcwmuFB)
+
+
+# react vscode 세팅
+```
+npm install -g create-react-app
+create-react-app app
+cd app
+npm start
+```
+
+4강
---
+++ Javascript/React/React 이론부터 실전까지/app/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+/*
+class HelloReact extends React.Component{
+  render(){
+    return(
+      <h1>Hello React!</h1>
+    )
+  }
+}
+
+function formatInfo(student){
+  return student.name + "[" + student.id + "]";
+}
+
+const student = {
+  id:"20161607",
+  name: "marinelife",
+  color: "blue"
+};
+
+const element = (
+  <h3 class={student.color}>
+    {formatInfo(student)}
+  </h3>
+);
+
+ReactDOM.render(element,
+  document.getElementById('root'));
+*/
+
+/*
+function tick(){
+  const element = (
+    <h3>현재 시간은 [{new Date().toLocaleTimeString()}] 입니다.</h3>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+setInterval(tick, 1000);
+*/
