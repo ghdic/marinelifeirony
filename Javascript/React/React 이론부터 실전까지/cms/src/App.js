be6fd69cc1f01e6d22a646c82828055d8634c131
ghdic <ghdic@naver.com>
2020-04-17 12:46:30 +0900

    create project

---
+++ Javascript/React/React 이론부터 실전까지/cms/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+  render(){
+    return(
+      <div className="gray-background">
+        <img src={logo} lat="logo"/>
+        <h2>Let's develop management system!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
