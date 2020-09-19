7dd53ab715cb01d6d7ebfe8f981e77dc39312806
ghdic <ghdic@naver.com>
2020-09-19 16:07:59 +0900

    Qt example add

--- Legacy/오늘의코드/파이썬andor이해하기/andor.py
+++ Legacy/오늘의코드/파이썬andor이해하기/andor.py
@@ -1,39 +1,53 @@
-# 연산자 and > or 우선순위
-
-# 컴퓨터가 False로 인식하는 것들
-"" # 빈 문자열
-None # 아무것도 없는 빈값
-0 # 숫자 0
-[] # 비어있는 리스트, 딕셔너리, 튜플
-{}
-()
-
-# True, False는 무슨값일까?
-int(True)
-int(False)
-str(True)
-str(False)
-print(True.__str__())
-if str(False):
-    print("hi")
-print(True + True)
-# 비교문제에서 반환하는건?
-
-# 숫자 number
-1 and 0 or 1 and 1
-1 and 1 or 0 and 1
-0 and 1 or 0 and 1
-0 or 1 or 0 and 1
+# # 연산자 and > or 우선순위
 
 # and or 과 비트연산자 & | 의 다른점
-1 & 0 | 1 & 1
-1 & 1 | 0 & 1
-0 & 1 | 0 & 1
-0 | 1 | 0 & 1
+# x = 7
+# print(x > 5 and x < 10)
+# print((x > 5) & (x < 10))
+# print(4 and 10)
+# print(4 and 10 or 20)
+# print(4 and 10 and 20)
+# print(4 & 10)
+# 1 & 0 | 1 & 1
+# 1 & 1 | 0 & 1
+# 0 & 1 | 0 & 1
+# 0 | 1 | 0 & 1
 
-# 문자열 반환
-"python" and ""
-"python" or ""
-"" or "python"
 
-# 단락 평가
+# # 컴퓨터가 False로 인식하는 것들
+# "" # 빈 문자열
+# None # 아무것도 없는 빈값
+# 0 # 숫자 0
+# [] # 비어있는 리스트, 딕셔너리, 튜플
+# {}
+# ()
+
+# # True, False는 무슨값일까?
+# print(int(True))
+# print(int(False))
+# print(str(True))
+# print(str(False))
+# print(True.__str__())
+# if str(False):
+#     print("False Str -> True")
+# print(True + True)
+
+
+# # 문자열 반환
+# print("python" and "")
+# print("python" or "")
+# print("" or "python")
+
+
+# False and False or "hello" or "world"
+# # 단락 평가
+# print(1 and 0 or 2 and 3)
+# print(1 and 2 or 0 and 3)
+# print(0 and 2 or 0 and 3)
+# print(0 or 1 or 0 and 2)
+
+try:
+    output = eval("2*40/0")
+except Exception as e:
+    output = e
+print(output)
