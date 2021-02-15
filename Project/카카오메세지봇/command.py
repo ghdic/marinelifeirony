0c457619e49c59f2143ecf8cabecb0ea8d5ec084
ghdic <ghdic@naver.com>
2021-02-15 18:58:27 +0900

    기능 조금 추가

---
+++ Project/카카오메세지봇/command.py
@@ -0,0 +1,14 @@
+
+class Fuction:
+    """
+    !help # 명령어 출력
+    !인사
+    !stock [Ticker] # 티커 가격 정보를 출력
+    !info [Ticker]
+    !history [Ticker] [Term(day)] # open, close data 던져줌
+    !forecast [Ticker]
+    !shareholder [Ticker]
+    !news [Ticker]
+    !chart [Ticker]
+    !comprehensive [Ticker] 통합
+    """
