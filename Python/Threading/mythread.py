3c9935f710c7dbf12feb00a8f173698648c2639a
ghdic <ghdic@naver.com>
2021-03-04 21:00:09 +0900

    몇초후 종료하는 스레드 예제 추가

--- Python/Threading/mythread.py
+++ Python/Threading/mythread.py
@@ -44,39 +44,100 @@
 # print(f"Finish in {round(finish-start)} second")
 
 
-import concurrent.futures
-import logging
-import queue
-import random
-import threading
-import time
+# import concurrent.futures
+# import logging
+# import queue
+# import random
+# import threading
+# import time
+#
+# def producer(queue, event):
+#     """ 네트워크로부터 넘버를 받음 """
+#     while not event.is_set():
+#         message = random.randint(1, 101)
+#         logging.info("Producer got message: %s", message)
+#         queue.put(message)
+#     logging.info("Producer received event. Exiting")
+#
+# def consumer(queue, event):
+#     """ 데이터베이스에 숫자를 저장함 """
+#     while not event.is_set() or not queue.empty():
+#         message = queue.get()
+#         logging.info("Consumer storing message: %s (size=%d)", message, queue.qsize())
+#
+#     logging.info("Consumer received event. Exiting")
+#
+# if __name__ == "__main__":
+#     format = "%(asctime)s : %(message)s"
+#     logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+#     pipeline = queue.Queue(maxsize=10)
+#     event = threading.Event()
+#
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+#         executor.submit(producer, pipeline, event)
+#         executor.submit(consumer, pipeline, event)
+#
+#         time.sleep(0.1)
+#         logging.info("Main: about to set event")
+#         event.set()
 
-def producer(queue, event):
-    """ 네트워크로부터 넘버를 받음 """
-    while not event.is_set(): 
-        message = random.randint(1, 101)
-        logging.info("Producer got message: %s", message)
-        queue.put(message)
-    logging.info("Producer received event. Exiting")
 
-def consumer(queue, event):
-    """ 데이터베이스에 숫자를 저장함 """
-    while not event.is_set() or not queue.empty():
-        message = queue.get()
-        logging.info("Consumer storing message: %s (size=%d)", message, queue.qsize())
+# # 시간이 지난 후 스레드 종료하기(원하는곳에서 종료가능)
+# import threading
+# import time
+# from apscheduler.schedulers.background import BackgroundScheduler
+#
+#
+# def start():
+#     print("start")
+#     t = threading.Thread(target=run, args=("run함수",))
+#     t.start()
+#     return t
+#
+#
+# def run(arg):
+#     print("run")
+#     t = threading.currentThread()
+#     while getattr(t, 'do_run', True):  # do_run 값이 정의되지 않는 경우 True 반환
+#         print("%s 작동중~~" % arg)
+#         time.sleep(1)
+#
+#
+# def stop(t):
+#     t.do_run = False
+#     t.join()
+#     print("stop")
+#     sched.remove_job('job1')
+#
+#
+# sched = BackgroundScheduler()
+# sched.start()
+#
+# t = start()
+# sched.add_job(stop, 'interval', seconds=5, args=(t,), id='job1')
+#
+# while True:
+#     print('메인 스레드 도는 중...')
+#     time.sleep(1)
 
-    logging.info("Consumer received event. Exiting")
-
-if __name__ == "__main__":
-    format = "%(asctime)s : %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-    pipeline = queue.Queue(maxsize=10)
-    event = threading.Event()
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-        executor.submit(producer, pipeline, event)
-        executor.submit(consumer, pipeline, event)
-
-        time.sleep(0.1)
-        logging.info("Main: about to set event")
-        event.set()
+# # 멀티프로세싱을 사용하는 경우는 강제로 종료 가능..
+# import multiprocessing
+# from multiprocessing import freeze_support
+# import time
+#
+#
+# def run(arg):
+#     print("run")
+#     while True:
+#         print("%s 작동중~~" % arg)
+#         time.sleep(1)
+#
+#
+# if __name__ == '__main__':
+#     freeze_support()
+#     proc = multiprocessing.Process(target=run, args=('run함수',))
+#     proc.start()
+#     time.sleep(5)
+#     proc.terminate()
+#     proc.join()
+#     print("종료")
