772d60a52b287eff4d650f7f63da147161c19282
ghdic <ghdic77@gmail.com>
2021-07-12 10:13:39 +0900

    리액트 api 연동 기본

---
+++ Web/React/api-integrate/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Users from "./Users";
+
+
+
+function App() {
+  return (
+    <Users />
+  );
+}
+
+export default App;
---
+++ Web/React/api-integrate/src/Users.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+
+function Users() {
+    const [users, setUsers] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchUsers = async () => {
+        try {
+            setUsers(null);
+            setError(null);
+            setLoading(true);
+            const response = await axios.get(
+                'https://jsonplaceholder.typicode.com/users/'
+            );
+            setUsers(response.data);
+        } catch (e) {
+            setError(e);
+        }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    if(loading) return <div>로딩중 ..</div>
+    if(error) return <div>에러가 발생했습니다</div>
+    if(!users) return null;
+    return <>
+    <ul>
+        {users.map(user => (
+            <li key={user.id}>
+                {user.username} ({user.name})
+            </li>
+        ))}
+    </ul>
+    <button onClick={fetchUsers}>다시 불러오기</button>
+    </>
+}
+
+export default Users;
