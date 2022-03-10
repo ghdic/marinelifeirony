cde19f8e9836f0b8f547a7c1818515b904d0efb5
ghdic <ghdic77@gmail.com>
2022-03-10 12:28:46 +0900

    smtplib 학습

---
+++ Python/smtplib/mysmtplib.py
@@ -0,0 +1,61 @@
+import smtplib
+from pathlib import Path
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.utils import formatdate
+from email import encoders
+import os
+
+
+def send_mail(send_from, send_to, subject, message, mtype='plain', files=[],
+              server="localhost", port=587, username='', password='',
+              use_tls=True):
+    """Compose and send email with provided info and attachments.
+
+    Args:
+        send_from (str): from name
+        send_to (list[str]): to name(s)
+        subject (str): message title
+        message (str): message body
+        mtype (str): choose type 'plain' or 'html'
+        files (list[str]): list of file paths to be attached to email
+        server (str): mail server host name
+        port (int): port number
+        username (str): server auth username
+        password (str): server auth password
+        use_tls (bool): use TLS mode
+    """
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = ', '.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message, mtype))
+
+    for path in files:
+        part = MIMEBase('application', "octet-stream")
+        with open(path, 'rb') as file:
+            part.set_payload(file.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition',
+                        'attachment; filename={}'.format(Path(path).name))
+        msg.attach(part)
+
+    smtp = smtplib.SMTP(server, port)
+    if use_tls:
+        smtp.starttls()
+    smtp.login(username, password)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.quit()
+
+
+id = os.environ['id']
+pw = os.environ['pw']
+email = os.environ['email']
+
+# 네이버의 경우 server='smtp.naver.com'
+send_mail(send_from=email, send_to=['ghdic77@gmail.com', 'ghdic@naver.com'],
+          subject='smtp 테스트 발송 메세지 입니다', message=f'<h1>안녕하세요</h1>{id}입니다', files=['temp.txt'],
+          mtype='html', server='smtp.gmail.com', username=email, password=pw)
