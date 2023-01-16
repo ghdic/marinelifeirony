bee0185f8d14ec55497ed5d1b0f9ec73d8a4c79f
marinelife <ghdic77@gmail.com>
2023-01-16 21:56:14 +0900

    cache clear

--- Project/randomRollet/random_rollet.py
+++ Project/randomRollet/random_rollet.py
@@ -24,6 +24,7 @@ class RandomRollet:
             print(f'질문: {target_quiz}')
             time.sleep(0.1)
         del self.quiz[target_index]
+        print(self.quiz)
         print(len(self.quiz))
         return target_quiz
 
@@ -37,8 +38,8 @@ class RandomRollet:
 
 
 if __name__ == '__main__':
+    randomRollet = RandomRollet(quiz_type="algorithm")
     while True:
-        randomRollet = RandomRollet(quiz_type="algorithm")
         while True:
             quiz = randomRollet.shuffle_quiz()
             event = input("")  # waiting..
